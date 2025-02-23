0b86d02d11b03fc591e20e93229e431481227bee
jeff <912833721@qq.com>
2025-02-23 20:36:30 +0800

    学习笔记补充-Flutter charter11.1（网络）

--- lib/chapter11/http.dart
+++ lib/chapter11/http.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HttpTestRoute extends StatefulWidget {
   const HttpTestRoute({Key? key}) : super(key: key);
@@ -13,6 +13,8 @@ class HttpTestRoute extends StatefulWidget {
 class _HttpTestRouteState extends State<HttpTestRoute> {
   bool _loading = false;
   String _text = "";
+  String startTime = "";
+  String endTime = "";
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +25,29 @@ class _HttpTestRouteState extends State<HttpTestRoute> {
             child: const Text("获取百度首页"),
             onPressed: _loading ? null : request,
           ),
+          TextButton(onPressed: () {
+
+          }, child: Text('$startTime --> $endTime', style: TextStyle(fontSize: 16.0, color: Colors.blue.shade700))),
           SizedBox(
             width: MediaQuery.of(context).size.width - 50.0,
             child: Text(_text.replaceAll(RegExp(r"\s"), "")),
-          )
+          ),
         ],
       ),
     );
   }
 
+  // Optimize: 同时注意dart的Future它相当于协程，不会阻塞UI线程，仅仅是这个“域”内等待获取结果
+  // Optimize: 验证的话，你看TextButton一直是可以点击的
   request() async {
     setState(() {
+      // 获取当前时间
+      startTime = DateFormat('HH:mm:ss').format(DateTime.now());
       _loading = true;
       _text = "正在请求...";
     });
     try {
+      await Future.delayed(Duration(seconds: 3));
       //创建一个HttpClient
       HttpClient httpClient = HttpClient();
       //打开Http连接
@@ -60,7 +70,9 @@ class _HttpTestRouteState extends State<HttpTestRoute> {
     } catch (e) {
       _text = "请求失败：$e";
     } finally {
+      // Optimize: 最终刷新一下是很关键，并且上面的异步操作都采用了await（相当于join等结果，直到一步步最后到这里时一定是有结果的，成功or失败）
       setState(() {
+        endTime = DateFormat('HH:mm:ss').format(DateTime.now());
         _loading = false;
       });
     }
